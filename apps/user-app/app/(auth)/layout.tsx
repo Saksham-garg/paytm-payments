9cc7af926ba7ab010a29e8c0ffaaa342e5d46779
Saksham-garg <sakshamgarg142@hotmail.com>
2024-07-23 22:26:35 +0530

    fix: css files and config for shadcn

--- apps/user-app/app/(auth)/layout.tsx
+++ apps/user-app/app/(auth)/layout.tsx
@@ -1,26 +1,26 @@
 "use client"
 import React from 'react'
 import Image from 'next/image'
-const  layout = ({children}: Readonly<{ children: React.ReactNode}>) => {
+const layout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
   return (
     <div className='grid grid-cols-2'>
-        <div className="flex items-center justify-center min-h-screen bg-[url('/bg-left.png')] w-full object-cover p-4">
-              <div className="text-white">
-                  <div className="flex items-center gap-3.5 justify-center">
-                      <img src='./logo.svg' alt='logo'/>
-                      <p className='font-normal text-3xl'>PayZee</p>
-                  </div>
-                  <Image 
-                    src="/payment-main.png"
-                    height={400}
-                    width={500}
-                    alt='main'
-                  />
-                  <p className='font-bold font-sans text-center text-3xl'>Make your payments at lightning fast.</p>
-                  <p className='font-thin font-sans text-center text-xl mt-3'>What you are waiting for, let's connect</p>
-              </div>
+      <div className="flex items-center justify-center min-h-screen bg-[url('/bg-left.png')] w-full object-cover p-4">
+        <div className="text-white">
+          <div className="flex items-center gap-3.5 justify-center">
+            <img src='./logo.svg' alt='logo' />
+            <p className='font-normal text-3xl'>PayZee</p>
+          </div>
+          <Image
+            src="/payment-main.png"
+            height={400}
+            width={500}
+            alt='main'
+          />
+          <p className='font-bold font-sans text-center text-3xl'>Make your payments at lightning fast.</p>
+          <p className='font-thin font-sans text-center text-xl mt-3'>What you are waiting for, let's connect</p>
         </div>
-        {children}
+      </div>
+      {children}
     </div>
   )
 }
--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -2,81 +2,30 @@
 module.exports = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "../../packages/ui/src/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   prefix: "",
   theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
-    },
     extend: {
       colors: {
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
-        primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
-        },
-        secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
-        },
-        destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
-        },
-        muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
-        },
-        accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
-        },
-        popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
-        },
-        card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
-        },
-      },
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
-      },
-      keyframes: {
-        "caret-blink": {
-          "0%,70%,100%": { opacity: "1" },
-          "20%,50%": { opacity: "0" },
-        },
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
-        },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-        "caret-blink": "caret-blink 1.25s ease-out infinite"
+        'difficulty-beginner': 'hsl(var(--difficulty-beginner) / <alpha-value>)',
+        'difficulty-easy': 'hsl(var(--difficulty-easy) / <alpha-value>)',
+        'difficulty-medium': 'hsl(var(--difficulty-medium) / <alpha-value>)',
+        'difficulty-hard': 'hsl(var(--difficulty-hard) / <alpha-value>)',
+        'difficulty-extreme': 'hsl(var(--difficulty-extreme) / <alpha-value>)',
+
+        'difficulty-beginner-dark': 'hsl(var(--difficulty-beginner-dark) / <alpha-value>)',
+        'difficulty-easy-dark': 'hsl(var(--difficulty-easy-dark) / <alpha-value>)',
+        'difficulty-medium-dark': 'hsl(var(--difficulty-medium-dark) / <alpha-value>)',
+        'difficulty-hard-dark': 'hsl(var(--difficulty-hard-dark) / <alpha-value>)',
+        'difficulty-extreme-dark': 'hsl(var(--difficulty-extreme-dark) / <alpha-value>)',
       },
     },
-  },
-  plugins: [require("tailwindcss-animate")],
+  }
 }
