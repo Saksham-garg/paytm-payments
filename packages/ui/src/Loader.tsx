55c1f4200e071df1548a460bb67a8af9922613ea
Saksham-garg <sakshamgarg142@hotmail.com>
2024-07-26 22:12:05 +0530

    Added loader when page navigates

---
+++ packages/ui/src/Loader.tsx
@@ -0,0 +1,13 @@
+import { Loader2 } from 'lucide-react'
+
+const Loader = () => {
+  return (
+    <div className='w-screen h-screen'>
+        <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 '>
+            <Loader2 className='animate-spin w-6 h-6'/> 
+        </div>
+    </div>
+  )
+}
+
+export default Loader
